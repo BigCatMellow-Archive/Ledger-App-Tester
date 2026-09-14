4c148c40dbe638b160e9901db4ed07cb190a58c1
BigCat Mellow <201203536+BigCatMellow@users.noreply.github.com>
2026-09-14 09:49:49 -0400

    Add Ledger V3 script entrypoint

---
+++ stationery/v3.js
@@ -0,0 +1,4 @@
+document.write('<script src="stationery/v3-core.js?v=1"><\/script>');
+document.write('<script src="stationery/v3-render.js?v=1"><\/script>');
+document.write('<script src="stationery/v3-actions.js?v=1"><\/script>');
+document.write('<script src="stationery/v3-wire.js?v=1"><\/script>');
